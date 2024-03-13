df3dc3ee7b4b58d9acba662d7a29fcea3ec1e79a
Iryna Kulba <i.kulba94@mail.ru>
2024-03-13 16:46:17 +0300

    add lesson1:task2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum1 = i1 + l1;
+Console.WriteLine(sum1);
+
+double d1 = 5.5;
+double sum2 = i1 + d1;
+Console.WriteLine(sum2);
+
+int product1 = i1 * i2;
+long product2 = i1*l1;
+double product3 = d1 * i1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+
+int quotient = i1/i2;
+Console.WriteLine(quotient);
+
+double d2=6;
+double quotient2 = i1/d2;
+Console.WriteLine(quotient2);
